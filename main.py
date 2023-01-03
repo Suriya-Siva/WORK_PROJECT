4d8f4f29905798acf471716299a5d50d2ee35acd
Suriya-Siva <119554650+Suriya-Siva@users.noreply.github.com>
2023-01-03 17:31:48 +0800

    progres

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 
 import requests
 from bs4 import BeautifulSoup
-import smtplib
 import pandas as pd
 
 
 
-headers = {
-    "User-agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
 
-URL ="https://software.cisco.com/download/home/286289286/type/282487503/release/10.6.3?catid=278875243"
-
-def ciscotest():
-
-    page = requests.get(URL, headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    version_info = soup.find_all('div', class_='row single-image-container')
-    print(len(version_info))
-    for i in version_info:
-        print((soup.find('span', {'class':'pointer fileDescOuter'})).text)
-        
+URL ="https://docs.fortinet.com/document/fortigate/7.0.9/fortios-release-notes/553516/change-log"
 
 
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.text,'lxml')
+soup
+table1 = soup.find('thead')
+table1
+
+headers = []
+for i in table1.find_all('th'):
+    title = i.text
+    headers.append(title)
+         
+
+print (headers)
     
 
-    # price
-    
-    
-ciscotest()
+   
